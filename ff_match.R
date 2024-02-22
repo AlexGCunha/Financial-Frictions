48e09dc6afb3caa2dc6730b9b400d80c027f8481
Alexandre Cunha <alexandregomesdacunha@gmail.com>
2024-02-22 16:38:28 -0300

    updated ff_match

--- ff_match.R
+++ ff_match.R
@@ -24,3 +24,25 @@ library(haven)
 library(readxl) 
 library(arrow)
 library(MatchIt)
+
+df = read_parquet('fin_fric_dataset.parquet')
+
+#Some modifications
+df = df %>% 
+  #calculate how many times a firm was inspected
+  group_by(cnpj8) %>% 
+  mutate(n_inspected = unique(year_decision),
+         n_inspected = ifelse(is.na(n_inspected),0,n_inspected)) %>% 
+  ungroup() %>% 
+  #filter companies that were inspected at most once
+  filter(n_inspected <=1) %>% 
+  #take the year the company was first inspected
+  group_by(cnpj_8) %>% 
+  mutate(inspected = max(c(0,inspected), na.rm = T),
+         year_decision = max(c(0,year_decision), na.rm = T)) %>% 
+  #filter
+    
+
+
+
+
